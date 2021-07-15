9927418fab238e46bb999af5afeff011b07ad480
Chechenque <43196753+chechenque@users.noreply.github.com>
2021-07-15 10:21:19 -0500

    Primer commit

---
+++ src/test/java/com/IHI/invoiceservice/InvoiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IHI.invoiceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
